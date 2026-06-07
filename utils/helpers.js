24fe714c17d630212c87e1d55521f6ba96c0299c
toolshub26 <tools.hub26@gmail.com>
2026-06-07 15:40:08 +0530

    Added helper utilities
    Created helpers.js utility functions

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+function escapeHTML(str) {
+  str = String(str ?? "");
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
