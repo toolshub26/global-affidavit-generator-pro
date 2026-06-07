d6b4287d77251421e448ba308d3a75f3831018e8
toolshub26 <tools.hub26@gmail.com>
2026-06-07 12:59:31 +0530

    Added service worker for offline support
    Added sw.js to enable offline caching and Progressive Web App functionality.

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "affidavit-pro-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./purposes.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
